48df4ba04aa3c0402a165d012f5c115e628d1094
marcobaldi97 <60588169+marcobaldi97@users.noreply.github.com>
2021-05-20 16:42:06 -0300

    Cool comments.

--- client/src/products/ItemPage.js
+++ client/src/products/ItemPage.js
@@ -1,5 +1,6 @@
 import React   from 'react';
-import { Card, Button, Row, Container, Col, Carousel, CarouselItem } from 'react-bootstrap';
+import SingleCommentViewer from '../comments/SingleCommentViewer'
+import { Card, Button, Row, Container, Col, Carousel } from 'react-bootstrap';
 
 class ItemPage extends React.Component {
     constructor(props){
@@ -18,34 +19,34 @@ class ItemPage extends React.Component {
                 <Card className="cardFather" style={{}}>
                 <Container fluid>
                     <Row>
-                        <Col>
-                            <Carousel fade className="itemImg">
-                            <Carousel.Item>
-                                <img
-                                className="d-block w-100 itemImg"
-                                src="https://http2.mlstatic.com/D_NQ_NP_650934-MLB31124466602_062019-O.jpg"
-                                alt="First slide"
-                                />
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                className="d-block w-100"
-                                src="https://i.pinimg.com/originals/21/f7/d7/21f7d7bce9eaeb6437011b33ee5f0454.png"
-                                alt="Second slide"
-                                />
-                            </Carousel.Item>
-                            </Carousel>
-                            <Card.Body>
-                                <Card.Title className="itemTitle">{this.state.productName}</Card.Title>
-                                <Card.Text>
-                                    {this.state.productDesc}
-                                </Card.Text>
-                                <Button variant="primary primaryAlter">Buy!</Button>
-                            </Card.Body>
-                        </Col>
+                    <Col>
+                        <Carousel fade className="itemImg">
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100 itemImg"
+                            src="https://http2.mlstatic.com/D_NQ_NP_650934-MLB31124466602_062019-O.jpg"
+                            alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src="https://i.pinimg.com/originals/21/f7/d7/21f7d7bce9eaeb6437011b33ee5f0454.png"
+                            alt="Second slide"
+                            />
+                        </Carousel.Item>
+                        </Carousel>
+                        <Card.Body>
+                            <Card.Title className="itemTitle">{this.state.productName}</Card.Title>
+                            <Card.Text>
+                                {this.state.productDesc}
+                            </Card.Text>
+                            <Button variant="primary primaryAlter">Buy!</Button>
+                        </Card.Body>
+                    </Col>
                     </Row>
-                    
                 </Container>
+                <SingleCommentViewer author="Mike" question="Do they ship to Montevideo?" response="Sure dude." ableToResponse="false"></SingleCommentViewer>
                 </Card>
             </div>
 
