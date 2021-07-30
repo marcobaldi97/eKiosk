0c36f4cb67a02172c32cbfc4d9b2e86c47e809f4
marcobaldi97 <60588169+marcobaldi97@users.noreply.github.com>
2021-07-30 16:27:17 -0300

    updated and ts added.

--- client/src/myComponents/SingleCommentViewer/SingleCommentViewer.js
+++ client/src/myComponents/SingleCommentViewer/SingleCommentViewer.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
+import React from "react";
+import { Card } from "react-bootstrap";
 
 /***
  * Type: Dumb component.
  * Params: question, author, authorImg, response, ableToResponse (false if the poster is viewing this product).
  */
 class SingleCommentViewer extends React.Component {
-    render() {
-      return (
-        <div className="SingleCommentViewer">
-         <Card className="SingleCommentViewer">
-              <Card.Body>
-                  <Card.Title>
-                    <img src="https://www.w3schools.com/howto/img_avatar.png" className="Avatar"/>
-                    {this.props.question}
-                  </Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">{this.props.author}</Card.Subtitle>
-                  <Card.Text>{this.props.response}</Card.Text>
-                  <Card.Link disabled={this.props.ableToResponse}>Response</Card.Link>
-              </Card.Body>
-          </Card>
-        </div>
-      );
-    }  
-} export default SingleCommentViewer
+	render() {
+		return (
+			<div className="SingleCommentViewer">
+				<Card className="SingleCommentViewer">
+					<Card.Body>
+						<Card.Title>
+							<img src="https://www.w3schools.com/howto/img_avatar.png" className="Avatar" />
+							{this.props.question}
+						</Card.Title>
+						<Card.Subtitle className="mb-2 text-muted">{this.props.author}</Card.Subtitle>
+						<Card.Text>{this.props.response}</Card.Text>
+						<Card.Link disabled={this.props.ableToResponse}>Response</Card.Link>
+					</Card.Body>
+				</Card>
+			</div>
+		);
+	}
+}
+export default SingleCommentViewer;
