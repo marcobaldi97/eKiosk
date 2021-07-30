df19aec5175f8e7f93166dc8a0b2191b000be1fd
marcobaldi97 <60588169+marcobaldi97@users.noreply.github.com>
2021-07-30 16:18:19 -0300

    A little refactory

---
+++ client/src/myComponents/ItemPage/ItemPage.js
@@ -0,0 +1,58 @@
+import React from "react";
+import SingleCommentViewer from "../SingleCommentViewer/SingleCommentViewer";
+import { Card, Button, Row, Container, Col, Carousel } from "react-bootstrap";
+
+class ItemPage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			productName: "Bubbaloo",
+			productDesc: "Un chicle hecho de goma con sabor...",
+			productPrice: "",
+			productImgs: [],
+		};
+	}
+
+	render() {
+		return (
+			<div key="lol">
+				<Card className="cardFather" style={{}}>
+					<Container fluid>
+						<Row>
+							<Col>
+								<Carousel fade className="itemImg">
+									<Carousel.Item>
+										<img
+											className="d-block w-100 itemImg"
+											src="https://http2.mlstatic.com/D_NQ_NP_650934-MLB31124466602_062019-O.jpg"
+											alt="First slide"
+										/>
+									</Carousel.Item>
+									<Carousel.Item>
+										<img
+											className="d-block w-100"
+											src="https://i.pinimg.com/originals/21/f7/d7/21f7d7bce9eaeb6437011b33ee5f0454.png"
+											alt="Second slide"
+										/>
+									</Carousel.Item>
+								</Carousel>
+								<Card.Body>
+									<Card.Title className="itemTitle">{this.state.productName}</Card.Title>
+									<Card.Text>{this.state.productDesc}</Card.Text>
+									<Button variant="primary primaryAlter">Buy!</Button>
+								</Card.Body>
+							</Col>
+						</Row>
+					</Container>
+					<SingleCommentViewer
+						author="Mike"
+						question="Do they ship to Montevideo?"
+						response="Sure dude."
+						ableToResponse="false"
+					></SingleCommentViewer>
+				</Card>
+			</div>
+		);
+	}
+}
+export default ItemPage;
---
+++ client/src/myComponents/PublicatinSingleViewer/PublicationSingleViewer.js
@@ -0,0 +1,140 @@
+import React from "react";
+import axios from "axios";
+import { Container, Card, Row, Col, Button, Accordion } from "react-bootstrap";
+
+import DynamicTablePublications from "../myComponents/DynamicTablePublications";
+import PublicationWriter from "../PublicationWriter/PublicationWriter";
+
+class PublicationSingleViewer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			post_id: this.props.post_id,
+			imgsrc: "",
+			publication_content: "",
+			items: [],
+			publications: [],
+			loaded: false,
+		}; //this.state
+		this.refresh = this.refresh.bind(this);
+		this.printFather = this.printFather.bind(this);
+		this.initialLoad = this.initialLoad.bind(this);
+		this.deleteRecord = this.deleteRecord.bind(this);
+	}
+
+	deleteRecord(r) {
+		console.log("record " + r + " deleted");
+	}
+
+	initialLoad() {
+		if (!this.state.loaded) {
+			this.refresh();
+		}
+	}
+
+	refresh() {
+		try {
+			let aux = this.state.post_id;
+			let params = {
+				post_id: aux,
+			};
+			axios
+				.post("/publications/viewSinglePostResponses/", params) //url + parametros
+				.then((response) => {
+					this.setState({ publications: response.data.arrayOfPublications });
+				})
+				.catch((err) => {
+					console.log(err); //codigo de que hacer en caso de error.
+				});
+		} catch (err) {
+			console.log(err);
+		}
+		if (!this.state.loaded) {
+			try {
+				let aux = this.state.post_id;
+				let params = {
+					post_id: aux,
+				};
+				axios
+					.post("/publications/viewSinglePost/", params) //url + parametros
+					.then((response) => {
+						this.printFather(response.data.arrayOfPublications);
+					})
+					.catch((err) => {
+						console.log(err); //codigo de que hacer en caso de error.
+					});
+			} catch (err) {
+				console.log(err);
+			}
+		}
+	} //refresh.
+
+	printFather(elements) {
+		this.setState({ publication_content: elements[0].publication_content });
+		this.setState({ imgsrc: elements[0].image_file });
+		this.setState({ loaded: true });
+	} //printFather
+
+	changeImgSize(event) {
+		console.log("here!");
+		const currentClasses = event.target.className;
+		if (currentClasses === "flexBox fatherImg") event.target.className = "flexBox fatherImgExpand";
+		else event.target.className = "flexBox fatherImg";
+	}
+
+	render() {
+		this.initialLoad();
+		return (
+			<div id="PublicationSingleViewerContainer">
+				<div className="cardFather">
+					<Card>
+						<Card.Header>OP: {this.state.post_id}</Card.Header>
+						<Card.Body>
+							<Container fluid="md"></Container>
+							<blockquote className="blockquote mb-0">
+								<Row>
+									<Col md="auto">
+										<img className="flexBox fatherImg" src={this.state.imgsrc} alt="\(>.<)/" onClick={this.changeImgSize}></img>
+									</Col>
+									<Col md="auto">
+										<p>{this.state.publication_content}</p>
+									</Col>
+								</Row>
+							</blockquote>
+						</Card.Body>
+					</Card>
+				</div>
+				<div className="cardFather respondClass">
+					<Accordion>
+						<Card>
+							<Card.Header>
+								<Accordion.Toggle as={Button} variant="btn btn-outline-success" eventKey="0">
+									Respond
+								</Accordion.Toggle>
+							</Card.Header>
+							<Accordion.Collapse eventKey="0">
+								<Card.Body>
+									<PublicationWriter fatherId={this.state.post_id} />
+								</Card.Body>
+							</Accordion.Collapse>
+						</Card>
+					</Accordion>
+				</div>
+				<div className="cardFather">
+					<button className="btn btn-outline-success" onClick={this.refresh}>
+						Refresh
+					</button>
+				</div>
+				<div className="cardFather">
+					<DynamicTablePublications
+						className="c5pxmarginstop"
+						elements={this.state.publications}
+						refresh={this.refresh.bind()}
+						deleteRecord={this.deleteRecord.bind()}
+					></DynamicTablePublications>
+				</div>
+			</div>
+		);
+	}
+}
+export default PublicationSingleViewer;
